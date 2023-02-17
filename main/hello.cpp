bfa2888ff83e5484a9b36f20de6e81fa5d7d2ed6
Dhruv-Garodia <dhruvgarodia2002@gmail.com>
2023-02-17 20:20:15 +0530

    task2

---
+++ main/hello.cpp
@@ -0,0 +1,11 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello,from Dhruv Jyoti Garodia\n";
+  cout << "Hello, Jenkins\n";
+  cout << "PES1UG20CS527";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
